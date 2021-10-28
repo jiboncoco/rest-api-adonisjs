c1ee53261fa8bda1ba86563dd951bdd16320d6ac
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-10-28 14:41:42 +0700

    update function index and store

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,11 +2,16 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
+  public async index({ response }: HttpContextContract) {
+    const users = await User.all()
+    return response.status(200).json({ code: 200, status: 'success', data: users })
+  }
+
   public async store({ request, response }: HttpContextContract) {
     const input = request.only(['username', 'email'])
+
     try {
       const users = await User.create(input)
-
       return response.status(200).json({ code: 200, status: 'success', data: users })
     } catch (err) {
       return response.status(500).json({ code: 500, status: 'error', message: err.message })
