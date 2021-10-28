87da7c2bf5910aa769da06f4d4c33cd7ca508417
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-10-28 15:59:53 +0700

    update fixing

--- start/routes.ts
+++ start/routes.ts
@@ -23,4 +23,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.post('users', 'UsersController.store')
   Route.get('users', 'UsersController.index')
+  Route.get('users/:id', 'UsersController.show')
+  Route.put('users/:id', 'UsersController.update')
+  Route.delete('users/:id', 'UsersController.destroy')
 }).prefix('/api/v1')
